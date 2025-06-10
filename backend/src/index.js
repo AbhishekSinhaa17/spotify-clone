ac79ecb0e0978d9cb75060ad5debac7cd6ffc4fc
AbhishekSinhaa17 <abhiks1710@gmail.com>
2025-06-10 23:12:36 +0530

    frontend api updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -72,21 +72,19 @@ app.use("/api/stats", statRoutes);
 if (process.env.NODE_ENV === "production") {
   // Serve static files from the React frontend app
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
 
 //error handler
 app.use((err, req, res, next) => {
-  res
-    .status(500)
-    .json({
-      message:
-        process.env.NODE_ENV === "production"
-          ? "Internal Server Error"
-          : err.message,
-    });
+  res.status(500).json({
+    message:
+      process.env.NODE_ENV === "production"
+        ? "Internal Server Error"
+        : err.message,
+  });
 });
 
 httpServer.listen(PORT, () => {
