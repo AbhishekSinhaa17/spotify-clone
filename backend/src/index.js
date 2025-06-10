df26088d12f19dcf7924d8f66022a74a0f170aa2
Abhishek Kumar Sinha <165494965+AbhishekSinhaa17@users.noreply.github.com>
2025-06-10 23:45:48 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -72,7 +72,7 @@ app.use("/api/stats", statRoutes);
 if (process.env.NODE_ENV === "production") {
   // Serve static files from the React frontend app
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("/*", (req, res) => {
+  app.get("/*splat", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
